60a5e4e4f1536beb6dd839aa79f78eeaeb0989f6
Scott Sandman <scottesandman@gmail.com>
2019-05-20 17:57:31 -0400

    Create JavascriptTest.js

---
+++ JavascriptTest.js
@@ -0,0 +1,20 @@
+const str = "Dave Baker is an Elite {crew member|production crew member|video crew member}. This {designation|status|label|classification} by Assignment Desk is {an extra|an additional|a supplementary|yet another|one extra|one additional} {sign|mark|signal|indication|symbol|endorsement} of {quality|high quality|top quality|good quality|excellence|top notch quality|outstanding quality|superior quality|high-quality|value|superiority}. Elite {crews|crew members|production crew members|video crew members} {have been|are actually|are already|are entirely|have been completely} {vetted|checked} by Assignment Desk as {crew members|production crew members|video crew members|film staff} {that have been|who have been|which have been} on shoots {many times|often|frequently|repeatedly|on numerous occasions|again and again|many times before} with {quality|high quality|top notch|excellent} equipment. {You can trust that|You can rely on|You can be confident|You can depend on|Have confidence} that an Elite {crew|staff member} {will|will certainly|is going to|would certainly|will definitely} {bring|deliver|provide} {an extra|an additional} {level of|amount of|degree of} service on your shoot."
+
+function change(str) {
+  var arr = str.split(/[{}]+/)
+  for (i=0; i<arr.length; i++) {
+    if (arr[i].includes("|")) {
+      var arr2 = arr[i].split("|")
+      var limit = arr2.length
+      var rand = Math.floor(Math.random() * limit)
+      if (rand >= arr2.length) {
+        rand = arr2.length - 1
+      }
+      arr[i] = arr2[rand]
+    }
+  }
+  var newStr = arr.join("")
+  return newStr
+}
+
+console.log(change(str))
